7d5f43202eda580de478898a308cf2da6b669678
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-10-29 12:31:02 -0700

    Cargue ejercicio de Basic HTML
    con elementos img, <b>, <i> <u> y atributo alt

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title> Exercise 9</title>
+		<meta charset="utf-8">
+	</head>
+	<p><font color="red">Este es el primer parrafo. </font></p>
+	<body> 
+		<br><h1>Hi_Andrew</h1>
+		<br>
+		<h2>how are you?</h2>
+		<br><h3>I hope you are well</h3>
+		<br><h6>see you soon.</h6>
+	</body>
+	<br><p><font color="#2E8B57">Este es el segundo parrafo parrafo. </font></p>
+	<body> 
+		<br><h1>Hi_Andrew</h1>
+		<br><h2>how are you?</h2>
+		<br><h3>I hope you are well</h3>
+		<br><h6>see you soon.</h6>
+	</body>
+</html>
