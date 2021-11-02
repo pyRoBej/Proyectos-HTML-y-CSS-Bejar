9aab70868f76363c6818049b02e656a76642098d
pyRoBej <65612883+pyRoBej@users.noreply.github.com>
2021-11-02 15:14:41 -0700

    agregue hola mundo y mas parrafos

---
+++ .vscode/Hello_world.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, world';
+console.log(greeting);
+
+var greeting2 = 'welcome to node.js';
+console.log(greeting2);
+
+var sentence1 = 'learning how to debug code with the debugger';
+console.log(sentence1);
